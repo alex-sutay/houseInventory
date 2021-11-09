e8afd737ff06bdc920398a85619d0685603a2dc9
Alex Sutay <ads1429@rit.edu>
2021-11-09 16:35:10 +0000

    Added an example config file

---
+++ flask/exampleconfig.py
@@ -0,0 +1,8 @@
+# SQL settings
+sql_host = ''  # the IP of the sql server, 127.0.0.1 if on the same server
+sql_user = ''  # username to log into the sql server
+sql_pass = ''  # password for the user above
+sql_db = ''  # the name of the database in the server you're using
+
+# Flask stuff
+secret_key = b''  #encoded string to use as your flask secret key
